c6b9f112e05bc00330f2c32f7e8e22a2de40b3b8
Nick Hand <nhand@berkeley.edu>
2017-03-29 21:58:00 -0700

    rename

--- runtests.py
+++ runtests.py
@@ -1,6 +1,6 @@
-from mpi4py_test.mpi import Tester
+from runtests.mpi import Tester
 import sys
 import os.path
 
-tester = Tester(os.path.abspath(__file__), "mpi4py_test")
+tester = Tester(os.path.abspath(__file__), "runtests")
 tester.main(sys.argv[1:])
