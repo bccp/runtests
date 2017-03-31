ebc6828941b9a2a32d7399a1cc3b08be577033bb
Yu Feng <rainwoodman@gmail.com>
2017-03-30 22:04:40 -0700

    pop current dir from module search path.

--- runtests.py
+++ runtests.py
@@ -1,5 +1,8 @@
+# pop the current directory from search path
+# python interpreter adds this to a top level script
+# but we will likely have a name conflict (runtests.py .vs runtests package)
+import sys; sys.path.pop(0)
 from runtests import Tester
-import sys
 import os.path
 
 tester = Tester(os.path.abspath(__file__), "runtests")
