77cc070eb8337ee9fbe5ef305f6a58591a0574e6
AntoineSIMTEK <antoine.sanner@imtek.uni-freiburg.de>
2020-06-17 10:39:06 +0200

    remove deprecated comments

--- run-mpitests.py
+++ run-mpitests.py
@@ -1,6 +1,3 @@
-# pop the current directory from search path
-# python interpreter adds this to a top level script
-# but we will likely have a name conflict (runtests.py .vs runtests package)
 import sys;
 from runtests.mpi import Tester
 
